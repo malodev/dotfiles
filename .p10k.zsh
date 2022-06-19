f1d1b555bbf7897546eb4c43a6edb33fdfa38eb7
Mauro Longano <mauro@oruam.org>
2022-06-19 18:28:17 +0200

    Fix prompt p10k

--- .p10k.zsh
+++ .p10k.zsh
@@ -96,10 +96,10 @@
     nix_shell               # nix shell (https://nixos.org/nixos/nix-pills/developing-with-nix-shell.html)
     vi_mode                 # vi mode (you don't need this if you've enabled prompt_char)
     # vpn_ip                # virtual private network indicator
-    load                  # CPU load
-    disk_usage            # disk usage
-    ram                   # free RAM
-    ip                    # ip address and bandwidth usage for a specified network interface
+    # load                  # CPU load
+    # disk_usage            # disk usage
+    # ram                   # free RAM
+    # ip                    # ip address and bandwidth usage for a specified network interface
     # swap                  # used swap
     todo                    # todo items (https://github.com/todotxt/todo.txt-cli)
     # =========================[ Line #2 ]=========================
@@ -109,8 +109,8 @@
     taskwarrior             # taskwarrior task count (https://taskwarrior.org/)
     # public_ip             # public IP address
     # proxy                 # system-wide http/https/ftp proxy
-    # battery               # internal battery
-    # wifi                  # wifi speed
+    battery               # internal battery
+    wifi                  # wifi speed
     # example               # example user-defined segment (see prompt_example function below)
   )
 
