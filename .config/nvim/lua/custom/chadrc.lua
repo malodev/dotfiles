5ee467eb636ae5b06f43aa8f3d9688fffa7b5dba
Mauro Longano <mauro@oruam.org>
2023-06-30 18:36:57 +0200

    Change NVChad status line

--- .config/nvim/lua/custom/chadrc.lua
+++ .config/nvim/lua/custom/chadrc.lua
@@ -10,6 +10,15 @@ M.ui = {
 
   hl_override = highlights.override,
   hl_add = highlights.add,
+  nvdash = {
+    load_on_startup = true,
+  },
+  statusline = {
+    theme = "vscode_colored", -- default/vscode/vscode_colored/minimal
+    -- default/round/block/arrow separators work only for default statusline theme
+    -- round and block will work for minimal theme only
+    separator_style = "round",
+  },
 }
 
 M.plugins = "custom.plugins"
