23488be5390d1d8f070c8b3dab0cbcf50bb14842
Mauro Longano <mauro.longano@marconirovereto.it>
2023-12-30 02:27:39 +0100

    Aggiungi splash screen e ChatGPT plugins

--- .config/nvim.me/init.lua
+++ .config/nvim.me/init.lua
@@ -1,5 +1,5 @@
 require("options")
-require("keymaps")
 require("lazy-init")
+require("keymaps")
 
 
--- .config/nvim.me/lua/keymaps.lua
+++ .config/nvim.me/lua/keymaps.lua
@@ -1,24 +1,29 @@
 local opts = { noremap = true, silent = true }
 local keymap = vim.keymap
 local global = vim.g
-global.mapleader = ' '
-global.maplocalleader = ' '
+global.mapleader = " "
+global.maplocalleader = " "
 
 -- make CTRL + C behave exactly the same as ESC
-keymap.set('i', '<C-c>', '<ESC>',  { desc = 'Make CTRL + C behave exactly the same as ESC', silent = true, noremap = true })
+keymap.set(
+  "i",
+  "<C-c>",
+  "<ESC>",
+  { desc = "Make CTRL + C behave exactly the same as ESC", silent = true, noremap = true }
+)
 
 -- delete one word in insert mode (note that <C-h> sends the same ASCII escape sequence as <C-BS>)
-keymap.set('i', '<C-h>', '<C-w>', opts)
+keymap.set("i", "<C-h>", "<C-w>", opts)
 
 -- remap ^ and $ to H and L, respectively
-keymap.set('n', 'H', '^', opts)
-keymap.set('n', 'L', '$', opts)
+keymap.set("n", "H", "^", opts)
+keymap.set("n", "L", "$", opts)
 
 -- open up lazy.nvim UI
 -- keymap.set('n', '<leader>l', ':Lazy<CR>', opts)
 
 -- toggle undotree
-keymap.set('n', '<C-u>', ':UndotreeToggle<CR>', opts)
+keymap.set("n", "<C-u>", ":UndotreeToggle<CR>", opts)
 
 -- toggle nvim-tree
 -- keymap.set('n', '<C-n>', ':NvimTreeFindFileToggle<CR>', opts)
@@ -27,104 +32,130 @@ keymap.set('n', '<C-u>', ':UndotreeToggle<CR>', opts)
 -- keymap.set('n', '<C-d>', '<nop>', opts)
 
 -- close the current buffer
-keymap.set('n', '<C-x>', ':bd<CR>', opts)
+keymap.set("n", "<C-x>", ":bd<CR>", opts)
 
 -- quickly switch between buffers
-keymap.set('n', '<', ':bp<CR>', opts)
-keymap.set('n', '>', ':bn<CR>', opts)
+keymap.set("n", "<", ":bp<CR>", opts)
+keymap.set("n", ">", ":bn<CR>", opts)
 
 -- quickly switch between windows
-keymap.set('n', '<C-h>', '<C-w>h', opts)
-keymap.set('n', '<C-l>', '<C-w>l', opts)
+keymap.set("n", "<C-h>", "<C-w>h", opts)
+keymap.set("n", "<C-l>", "<C-w>l", opts)
 
 -- Toogle pin current buffer
-keymap.set('n', '<leader>bp', ':BufferLineTogglePin<CR>', { desc="Pin current buffer", silent = true, noremap = true })
+keymap.set(
+  "n",
+  "<leader>bp",
+  ":BufferLineTogglePin<CR>",
+  { desc = "Pin current buffer", silent = true, noremap = true }
+)
 
 -- Pick a buffer
-keymap.set('n', '<leader>pb', ':BufferLinePick<CR>', { desc="Pick a buffer", silent = true, noremap = true })
+keymap.set("n", "<leader>pb", ":BufferLinePick<CR>", { desc = "Pick a buffer", silent = true, noremap = true })
 
 -- save current buffer
-keymap.set('n', '<C-s>', ':w<CR>', { desc="Save current buffer", silent = true, noremap = true })
+keymap.set("n", "<C-s>", ":w<CR>", { desc = "Save current buffer", silent = true, noremap = true })
 
 -- write to all buffers
-keymap.set('n', '<leader>w', ':wa<CR>', { desc="Write all buffers", silent = true, noremap = true })
+keymap.set("n", "<leader>w", ":wa<CR>", { desc = "Write all buffers", silent = true, noremap = true })
 
 -- quit all buffers
-keymap.set('n', '<leader>q', ':qa<CR>', opts)
+keymap.set("n", "<leader>q", ":qa<CR>", opts)
 
 -- force quit all buffers
-keymap.set('n', '<leader>fq', ':qa!<CR>', opts)
+keymap.set("n", "<leader>fq", ":qa!<CR>", opts)
 
 -- clear search term when centering the cursor
-keymap.set('n', 'zz', 'zz:noh<CR>', opts)
+keymap.set("n", "zz", "zz:noh<CR>", opts)
 
 -- toggle transparency
-keymap.set('n', '<C-t>', ':TransparentToggle<CR>', opts)
+keymap.set("n", "<C-t>", ":TransparentToggle<CR>", opts)
 
 -- open up Themery
 -- keymap.set('n', '<leader>t', ':Themery<CR>', opts)
 
 -- replace current word
-keymap.set('n', '<leader>s', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 -- make current file an executable
 -- keymap.set('n', '<leader>x', '<cmd>!chmod +x %<CR>', opts)
 
 -- keep cursor at front when appending lines below
-keymap.set('n', 'J', 'mzJ`z', opts)
+keymap.set("n", "J", "mzJ`z", opts)
 
 -- select entire file with CTRL + A
-keymap.set('n', '<C-a>', 'ggVG', opts)
+keymap.set("n", "<C-a>", "ggVG", opts)
 
 -- indent and outdent lines quickly
-keymap.set('n', '<TAB>', '>>', opts)
-keymap.set('n', '<S-TAB>', '<<', opts)
+keymap.set("n", "<TAB>", ">>", opts)
+keymap.set("n", "<S-TAB>", "<<", opts)
 
 -- search movement keeps cursor in middle
-keymap.set('n', 'n', 'nzzzv', opts)
-keymap.set('n', 'N', 'Nzzzv', opts)
+keymap.set("n", "n", "nzzzv", opts)
+keymap.set("n", "N", "Nzzzv", opts)
 
 -- vertical movement keeps cursor in middle
-keymap.set('n', '<C-j>', '<C-d>zz', opts)
-keymap.set('n', '<C-k>', '<C-u>zz', opts)
+keymap.set("n", "<C-j>", "<C-d>zz", opts)
+keymap.set("n", "<C-k>", "<C-u>zz", opts)
 
 -- creates a new line below the cursor and goes back into normal mode
-keymap.set('n', '<CR>', 'o<Esc>', opts)
+keymap.set("n", "<CR>", "o<Esc>", opts)
 
 -- creates a new line above the cursor and goes back into normal mode
-keymap.set('n', '<A-CR>', 'O<Esc>', opts)
+keymap.set("n", "<A-CR>", "O<Esc>", opts)
 
 -- quick resizing of buffers
-keymap.set('n', '<C-up>', ':resize -2<cr>', opts)
-keymap.set('n', '<C-down>', ':resize +2<cr>', opts)
-keymap.set('n', '<C-left>', ':vertical resize -2<cr>', opts)
-keymap.set('n', '<C-Right>', ':vertical resize +2<CR>', opts)
+keymap.set("n", "<C-up>", ":resize -2<cr>", opts)
+keymap.set("n", "<C-down>", ":resize +2<cr>", opts)
+keymap.set("n", "<C-left>", ":vertical resize -2<cr>", opts)
+keymap.set("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
 -- copy into system clipboard with CTRL + C
-keymap.set('v', '<C-c>', '"+y', opts)
+keymap.set("v", "<C-c>", '"+y', opts)
 
 -- copy into host system clipboard with <leader>y
-keymap.set('v', '<leader>y', '"*y', opts)
+keymap.set("v", "<leader>y", '"*y', opts)
 
 -- prevent x from copying over Vim clipboard
-keymap.set('n', 'x', '"_x', opts)
+keymap.set("n", "x", '"_x', opts)
 
 -- indent and outdent lines in visual mode
-keymap.set('v', '<TAB>', '<S->>gv', opts)
-keymap.set('v', '<S-TAB>', '<S-<>gv', opts)
+keymap.set("v", "<TAB>", "<S->>gv", opts)
+keymap.set("v", "<S-TAB>", "<S-<>gv", opts)
 
 -- the greatest remap ever (Primeagen)
-keymap.set('v', '<leader>p', '"_dP', opts)
+keymap.set("v", "<leader>p", '"_dP', opts)
 
 -- move lines around
-keymap.set('v', 'J', ":m '>+1<CR>gv=gv", opts)
-keymap.set('v', 'K', ":m '<-2<CR>gv=gv", opts)
+keymap.set("v", "J", ":m '>+1<CR>gv=gv", opts)
+keymap.set("v", "K", ":m '<-2<CR>gv=gv", opts)
 
 -- vertical movement keeps cursor in middle (visual mode)
-keymap.set('v', '<C-j>', '<C-d>zz', opts)
-keymap.set('v', '<C-k>', '<C-u>zz', opts)
+keymap.set("v", "<C-j>", "<C-d>zz", opts)
+keymap.set("v", "<C-k>", "<C-u>zz", opts)
 
 -- prevent incrementing numbers in file (this is actually horrible)
-keymap.set('v', '<C-a>', 'ggVG', opts)
+keymap.set("v", "<C-a>", "ggVG", opts)
 
-keymap.set('v', '<C-d>', '"+ygvd', opts)
+keymap.set("v", "<C-d>", '"+ygvd', opts)
+-- WhichKey mappings
+local wk = require("which-key")
+
+wk.register({
+  c = {
+    name = "ChatGPT",
+    c = { "<cmd>ChatGPT<CR>", "ChatGPT" },
+    e = { "<cmd>ChatGPTEditWithInstruction<CR>", "Edit with instruction", mode = { "n", "v" } },
+    g = { "<cmd>ChatGPTRun grammar_correction<CR>", "Grammar Correction", mode = { "n", "v" } },
+    t = { "<cmd>ChatGPTRun translate<CR>", "Translate", mode = { "n", "v" } },
+    k = { "<cmd>ChatGPTRun keywords<CR>", "Keywords", mode = { "n", "v" } },
+    d = { "<cmd>ChatGPTRun docstring<CR>", "Docstring", mode = { "n", "v" } },
+    a = { "<cmd>ChatGPTRun add_tests<CR>", "Add Tests", mode = { "n", "v" } },
+    o = { "<cmd>ChatGPTRun optimize_code<CR>", "Optimize Code", mode = { "n", "v" } },
+    s = { "<cmd>ChatGPTRun summarize<CR>", "Summarize", mode = { "n", "v" } },
+    f = { "<cmd>ChatGPTRun fix_bugs<CR>", "Fix Bugs", mode = { "n", "v" } },
+    x = { "<cmd>ChatGPTRun explain_code<CR>", "Explain Code", mode = { "n", "v" } },
+    r = { "<cmd>ChatGPTRun roxygen_edit<CR>", "Roxygen Edit", mode = { "n", "v" } },
+    l = { "<cmd>ChatGPTRun code_readability_analysis<CR>", "Code Readability Analysis", mode = { "n", "v" } },
+  },
+})
