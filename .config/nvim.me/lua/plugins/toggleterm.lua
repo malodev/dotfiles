d7335f066ee6e5d3f2c9af8ca49279009d166c58
Mauro Longano <mauro@oruam.org>
2023-12-31 02:49:54 +0100

    Aggiungi un comando per aprire lazygit con toggleterm

--- .config/nvim.me/lua/plugins/toggleterm.lua
+++ .config/nvim.me/lua/plugins/toggleterm.lua
@@ -1,22 +1,46 @@
 return {
-    'akinsho/toggleterm.nvim',
-    event = 'VimEnter',
-    config = function()
-        local status_ok, toggleterm = pcall(require, 'toggleterm')
+  "akinsho/toggleterm.nvim",
+  event = "VimEnter",
+  config = function()
+    local status_ok, toggleterm = pcall(require, "toggleterm")
 
-        if not status_ok then
-            return
-        end
-
-        toggleterm.setup({
-            size = 13,
-            open_mapping = [[<c-\>]],
-            shade_filetypes = {},
-            shade_terminals = true,
-            shading_factor = '1',
-            start_in_insert = true,
-            persist_size = true,
-            direction = 'horizontal',
-        })
+    if not status_ok then
+      return
     end
+
+    toggleterm.setup({
+      size = 13,
+      open_mapping = [[<c-\>]],
+      shade_filetypes = {},
+      shade_terminals = true,
+      shading_factor = "1",
+      start_in_insert = true,
+      persist_size = true,
+      direction = "horizontal",
+    })
+    local Terminal = require("toggleterm.terminal").Terminal
+    local lazygit = Terminal:new({
+      cmd = "lazygit",
+      dir = "git_dir",
+      direction = "float",
+      float_opts = {
+        border = "double",
+      },
+      -- function to run on opening the terminal
+      on_open = function(term)
+        vim.cmd("startinsert!")
+        vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
+      end,
+      -- function to run on closing the terminal
+      on_close = function(term)
+        vim.cmd("startinsert!")
+      end,
+    })
+
+    function _lazygit_toggle()
+      lazygit:toggle()
+    end
+
+    vim.api.nvim_set_keymap("n", "<leader>gl", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true, desc="Toggle lazygit" })
+  end,
 }
