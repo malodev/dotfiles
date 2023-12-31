70942108fb57212e3641496583df810570ff128d
Mauro Longano <mauro@oruam.org>
2023-12-31 02:48:50 +0100

    Aggiungi la trasparenza al tema

--- .config/nvim.me/lua/plugins/catppuccin.lua
+++ .config/nvim.me/lua/plugins/catppuccin.lua
@@ -1,16 +1,16 @@
-return
-{
+return {
   "catppuccin/nvim",
   name = "catppuccin",
   priority = 1000,
   -- options for catppuccin. This will automatically call `require("catppuccin").setup(opts)`
   --   opts = {
   opts = {
-    flavour = "mocha",   -- latte, frappe, macchiato, mocha
-    background = {       -- :h background
+    flavour = "mocha", -- latte, frappe, macchiato, mocha
+    background = {   -- :h background
       light = "latte",
       dark = "mocha",
     },
+    transparent_background = true, -- disables setting the background color.
     styles = {                   -- Handles the styles of general hi groups (see `:h highlight-args`):
       comments = { "italic" },   -- Change the style of comments
       conditionals = { "italic" },
@@ -25,11 +25,16 @@ return
       types = {},
       operators = {},
     },
+    integrations = {
+      cmp = true,
+      treesitter = true,
+      telescope = { enabled = true },
+      harpoon = true,
+    },
   },
 
   config = function(_, opts)
     require("catppuccin").setup(opts)
-    vim.cmd.colorscheme "catppuccin"
+    vim.cmd.colorscheme("catppuccin")
   end,
-
 }
