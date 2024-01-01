a24e314087993cf9e9e9acdc50624e1abe042e22
Mauro Longano <mauro@oruam.org>
2024-01-01 01:53:29 +0100

    Add a plugin for toggle comments

--- .config/nvim.me/lua/plugins/editing.lua
+++ .config/nvim.me/lua/plugins/editing.lua
@@ -1,28 +1,35 @@
 return {
-  {
-    "windwp/nvim-autopairs", -- plugin 1
-    event = { "InsertEnter" },
-    config = function()
-      local npairs_status_ok, npairs = pcall(require, "nvim-autopairs")
-      if not npairs_status_ok then
-        return
-      end
+	{
+		"windwp/nvim-autopairs", -- plugin 1
+		event = { "InsertEnter" },
+		config = function()
+			local npairs_status_ok, npairs = pcall(require, "nvim-autopairs")
+			if not npairs_status_ok then
+				return
+			end
 
-      npairs.setup({})
-    end,
-  },
-  {
-    "windwp/nvim-ts-autotag", -- plugin 2
-    event = { "InsertEnter" },
+			npairs.setup({})
+		end,
+	},
+	{
+		"windwp/nvim-ts-autotag", -- plugin 2
+		event = { "InsertEnter" },
 
-    config = function()
-      local autotag_status_ok, autotag = pcall(require, "nvim-ts-autotag")
+		config = function()
+			local autotag_status_ok, autotag = pcall(require, "nvim-ts-autotag")
 
-      if not autotag_status_ok then
-        return
-      end
+			if not autotag_status_ok then
+				return
+			end
 
-      autotag.setup({})
-    end,
-  },
+			autotag.setup({})
+		end,
+	},
+	{
+		"numToStr/Comment.nvim",
+		opts = {
+			-- add any options here
+		},
+		lazy = false,
+	},
 }
