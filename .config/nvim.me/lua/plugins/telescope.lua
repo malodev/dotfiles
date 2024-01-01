23b5ac1ae43064d23b2740aa021f18dd22cc8ac5
Mauro Longano <mauro@oruam.org>
2024-01-01 02:02:34 +0100

    Tried whithout success to view trouble error with telescope

--- .config/nvim.me/lua/plugins/telescope.lua
+++ .config/nvim.me/lua/plugins/telescope.lua
@@ -1,47 +1,44 @@
 return {
-  {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.5',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      "folke/trouble.nvim",
-    },
---    opts = {
---      defaults = {
---        mappings = {
---          i = { ["<c-t>"] = require("trouble.providers.telescope").open_with_trouble },
---          n = { ["<c-t>"] = require("trouble.providers.telescope").open_with_trouble },
---        },
---      },
---
---    },
-    config = function()
- --     require("telescope").setup(opts)
-      require('telescope').load_extension('fzf')
-    end,
-    keys = {
-      { "<leader>ff", "<cmd>Telescope find_files<CR>", desc = "telescope: find_files" },
-      { "<leader>fg", "<cmd>Telescope live_grep<CR>",  desc = "telescope: live_grep" },
-      { "<leader>fb", "<cmd>Telescope buffers<CR>",    desc = "telescope: buffers" },
-      { "<leader>fo", "<cmd>Telescope oldfiles<CR>",   desc = "telescope: oldfiles" },
-      { "<leader>fc", "<cmd>Telescope command_history <CR>",   desc = "telescope: oldfiles" },
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.5",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"folke/trouble.nvim",
+		},
+		config = function(_, opts)
+			local trouble = require("trouble.providers.telescope")
+			require("telescope").setup({
 
-    },
-  },
-  { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-  {
-    'nvim-telescope/telescope-ui-select.nvim',
-    config = function()
-      require("telescope").setup {
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown {
-            }
-          }
-        }
-      }
-      require("telescope").load_extension("ui-select")
-    end
-
-  }
+				defaults = {
+					mappings = {
+						i = { ["<c-t>"] = trouble.open_with_trouble },
+						n = { ["<c-t>"] = trouble.open_with_trouble },
+					},
+				},
+			})
+			require("telescope").load_extension("fzf")
+		end,
+		keys = {
+			{ "<leader>ff", "<cmd>Telescope find_files<CR>", desc = "telescope: find_files" },
+			{ "<leader>fg", "<cmd>Telescope live_grep<CR>", desc = "telescope: live_grep" },
+			{ "<leader>fb", "<cmd>Telescope buffers<CR>", desc = "telescope: buffers" },
+			{ "<leader>fo", "<cmd>Telescope oldfiles<CR>", desc = "telescope: oldfiles" },
+			{ "<leader>fc", "<cmd>Telescope command_history <CR>", desc = "telescope: oldfiles" },
+		},
+	},
+	{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
+			require("telescope").load_extension("ui-select")
+		end,
+	},
 }
