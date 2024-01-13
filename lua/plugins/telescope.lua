ecdabffa3c5af29519bb4dcd211d4aaab3907c41
Mauro Longano <mauro.longano@marconirovereto.it>
2024-01-13 12:22:22 +0100

    Add Telescope-media-files to preview images, etc..

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,46 +1,64 @@
 return {
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.5",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"folke/trouble.nvim",
-		},
-		config = function(_, opts)
-			local trouble = require("trouble.providers.telescope")
-			require("telescope").setup({
-
-				defaults = {
-					mappings = {
-						i = { ["<c-t>"] = trouble.open_with_trouble },
-						n = { ["<c-t>"] = trouble.open_with_trouble },
-					},
-				},
-			})
-			require("telescope").load_extension("fzf")
-      require('telescope').load_extension('prosession')
-		end,
-		keys = {
-			{ "<leader>ff", "<cmd>Telescope find_files<CR>", desc = "telescope: find_files" },
-			{ "<leader>fg", "<cmd>Telescope live_grep<CR>", desc = "telescope: live_grep" },
-			{ "<leader>fb", "<cmd>Telescope buffers<CR>", desc = "telescope: buffers" },
-			{ "<leader>fo", "<cmd>Telescope oldfiles<CR>", desc = "telescope: oldfiles" },
-			{ "<leader>fc", "<cmd>Telescope command_history <CR>", desc = "telescope: oldfiles" },
-			{ "<leader>pf", "<cmd>Telescope prosession <CR>", desc = "telescope: sessions" },
-		},
-	},
-	{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
-		config = function()
-			require("telescope").setup({
-				extensions = {
-					["ui-select"] = {
-						require("telescope.themes").get_dropdown({}),
-					},
-				},
-			})
-			require("telescope").load_extension("ui-select")
-		end,
-	},
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.5",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-lua/popup.nvim",
+      "folke/trouble.nvim",
+      "nvim-telescope/telescope-media-files.nvim",
+    },
+    config = function(_, opts)
+      local trouble = require("trouble.providers.telescope")
+      require("telescope").setup({
+        extensions = {
+          media_files = {
+            -- filetypes whitelist
+            -- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
+            -- filetypes = { "png", "webp", "jpg", "jpeg" },
+            -- find command (defaults to `fd`)
+            find_cmd = "rg",
+          },
+        },
+        defaults = {
+          mappings = {
+            i = { ["<c-t>"] = trouble.open_with_trouble },
+            n = { ["<c-t>"] = trouble.open_with_trouble },
+          },
+          layout_config = {
+            horizontal = {
+              preview_cutoff = 0,
+            },
+          },
+        },
+      })
+      require("telescope").load_extension("fzf")
+      require("telescope").load_extension("prosession")
+      require("telescope").load_extension("media_files")
+    end,
+    keys = {
+      { "<leader>ff", "<cmd>Telescope find_files<CR>",       desc = "telescope: find_files" },
+      { "<leader>fg", "<cmd>Telescope live_grep<CR>",        desc = "telescope: live_grep" },
+      { "<leader>fb", "<cmd>Telescope buffers<CR>",          desc = "telescope: buffers" },
+      { "<leader>fo", "<cmd>Telescope oldfiles<CR>",         desc = "telescope: oldfiles" },
+      { "<leader>fc", "<cmd>Telescope command_history <CR>", desc = "telescope: command_history" },
+      { "<leader>pf", "<cmd>Telescope prosession <CR>",      desc = "telescope: sessions" },
+      { "<leader>fp", "<cmd>Telescope prosession <CR>",      desc = "telescope: sessions" },
+      { "<leader>fi", "<cmd>Telescope media_files <CR>",      desc = "telescope: media_files" },
+    },
+  },
+  { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+    config = function()
+      require("telescope").setup({
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown({}),
+          },
+        },
+      })
+      require("telescope").load_extension("ui-select")
+    end,
+  },
 }
