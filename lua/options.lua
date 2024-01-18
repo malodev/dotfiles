434f3a1d89e3e98c31f564824f0334cda8e25998
Mauro Longano <mauro@oruam.org>
2024-01-18 01:28:43 +0100

    Set right mouse click to extend selection instead of showing menu

--- lua/options.lua
+++ lua/options.lua
@@ -4,6 +4,7 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set ignorecase")
 vim.cmd("set smartcase")
+vim.cmd("set mousemodel=extend")
 
 vim.g.mapleader = " "
 vim.opt.clipboard = "unnamedplus" -- use system clipboard
