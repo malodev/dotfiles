46d5a63188d174b3e30705cf46e96c7c54e2254b
Mauro Longano <mauro@oruam.org>
2024-01-18 21:39:59 +0100

    Tune lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,11 +1,50 @@
 -- palenight
 return {
-  'nvim-lualine/lualine.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
-  opts = {
-    options = {
-      icons_enabled = true,
-      theme = 'palenight',
-    },
-  }
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("lualine").setup({
+			options = {
+				theme = "palenight",
+				section_separators = { left = "", right = "" },
+				-- section_separators = { "", "" },
+				component_separators = { left = "", right = "" },
+				icons_enabled = true,
+			},
+			sections = {
+				lualine_a = { { "mode", icons_enabled = true, icon = "" } },
+				lualine_b = { "branch" },
+				lualine_c = { "filename" },
+				lualine_x = { "encoding", "fileformat", "filetype" },
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
+			inactive_sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch" },
+				lualine_c = { "filename" },
+				lualine_x = { "encoding", "fileformat", "filetype" },
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
+			winbar = {
+				lualine_a = {
+					{
+						"diff",
+						diff_color = {
+							-- Same color values as the general color option can be used here.
+							added = { fg = "#a6e3a1", bg = "#242324" }, -- Changes the diff's added color
+							modified = { fg = "#f9e2af", bg = "#242324" }, -- Changes the diff's modified color
+							removed = { fg = "#f38ba8", bg = "#242324" }, -- Changes the diff's removed color you
+						},
+						symbols = { added = " ", modified = "󰿡 ", removed = " " },
+					},
+				},
+				lualine_b = { { "diagnostic", sources = { "nvim_lsp", "vim_lsp" } } },
+				lualine_y = { "selectioncount", "searchcount" },
+				lualine_z = { { "mode", icons_enabled = true, icon = "" } },
+			},
+			extensions = {},
+		})
+	end,
 }
