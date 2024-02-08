c2b59b1f9e56ad22a71ed5b7750743d22b4866bd
Mauro Longano <mauro@oruam.org>
2024-02-08 10:12:04 +0100

    Add bash and regex to treesitter

--- lua/plugins/tresitter.lua
+++ lua/plugins/tresitter.lua
@@ -1,24 +1,26 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    local configs = require("nvim-treesitter.configs")
-    configs.setup({
-      ensure_installed = {
-        "c",
-        "lua",
-        "vim",
-        "javascript",
-        "typescript",
-        "tsx",
-        "css",
-        "html",
-        "markdown",
-        "markdown_inline",
-      },
-      sync_install = false,
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
-  end
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
+		configs.setup({
+			ensure_installed = {
+				"c",
+				"lua",
+				"vim",
+				"javascript",
+				"typescript",
+				"tsx",
+				"css",
+				"html",
+				"markdown",
+				"markdown_inline",
+				"bash",
+				"regex",
+			},
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
