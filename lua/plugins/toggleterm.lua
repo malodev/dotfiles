4e6d66872ba1dd82c1f84cd7fee7bf36aba14176
Mauro Longano <mauro@oruam.org>
2024-02-14 09:27:08 +0100

    Fix quit key for lazygit term

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -72,7 +72,13 @@ return {
 			-- function to run on opening the terminal
 			on_open = function(term)
 				vim.cmd("startinsert!")
-				vim.api.nvim_buf_set_keymap(term.bufnr, "t", "q", "<cmd>close<CR>", { noremap = true, silent = true })
+				vim.api.nvim_buf_set_keymap(
+					term.bufnr,
+					"t",
+					"<A-q>",
+					"<cmd>close<CR>",
+					{ noremap = true, silent = true }
+				)
 			end,
 			-- function to run on closing the terminal
 			on_close = function(term)
