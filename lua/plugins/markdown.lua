b615c81687e596c24be366fbd3762df93b2aaab2
Mauro Longano <mauro@oruam.org>
2024-02-14 09:30:42 +0100

    Tweak markdown

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,13 +1,16 @@
 return {
-    {
-        'iamcco/markdown-preview.nvim',
-        build = 'cd app && npm install',
-        -- using npm to install rather than the vim function leads to significantly faster startup time
-        init = function()
-            vim.g.mkdp_filetypes = { 'markdown' }
-        end,
-        config = function()
-            vim.keymap.set('n', '<leader>m', '<Plug>MarkdownPreviewToggle', { desc = 'Markdown Preview' })
-        end
-    },
+	{
+		"iamcco/markdown-preview.nvim",
+		build = "cd app && npm install",
+		-- using npm to install rather than the vim function leads to significantly faster startup time
+		init = function()
+			vim.g.mkdp_filetypes = { "markdown" }
+		end,
+		config = function()
+			vim.g.mkdp_port = 19400
+			vim.keymap.set("n", "<leader>mm", "<Plug>MarkdownPreview", { desc = " Start Markdown Preview" })
+			vim.keymap.set("n", "<leader>mh", "<Plug>MarkdownPreviewStop", { desc = " Halt Markdown Preview" })
+			vim.keymap.set("n", "<leader>mt", "<Plug>MarkdownPreviewToggle", { desc = " Start Markdown Preview" })
+		end,
+	},
 }
