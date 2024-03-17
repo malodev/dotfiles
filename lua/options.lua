c7fd6425e49cfb4a0973cea71a6dde2e3c5d664c
Mauro Longano <mauro@oruam.org>
2024-03-17 09:11:31 +0100

    Add autoreload of files when modified externaly

--- lua/options.lua
+++ lua/options.lua
@@ -8,8 +8,8 @@ vim.cmd("set mousemodel=extend")
 
 vim.g.mapleader = " "
 vim.opt.clipboard = "unnamedplus" -- use system clipboard
-vim.opt.number = true             -- show absolute number
-vim.opt.relativenumber = true     -- add numbers to each line on the left side
+vim.opt.number = true -- show absolute number
+vim.opt.relativenumber = true -- add numbers to each line on the left side
 
 -- the following lines are for relative number to be disabled in insert mode
 local autocmd = vim.api.nvim_create_autocmd
@@ -18,13 +18,21 @@ local augroup = vim.api.nvim_create_augroup
 local number_toggle = augroup("numbertoggle", { clear = true })
 
 autocmd({ "InsertLeave" }, {
-  pattern = "*",
-  command = "setlocal relativenumber",
-  group = number_toggle,
+	pattern = "*",
+	command = "setlocal relativenumber",
+	group = number_toggle,
 })
 
 autocmd({ "InsertEnter" }, {
-  pattern = "*",
-  command = "setlocal norelativenumber",
-  group = number_toggle,
+	pattern = "*",
+	command = "setlocal norelativenumber",
+	group = number_toggle,
+})
+
+-- auto-reload files when modified externally
+-- https://unix.stackexchange.com/a/383044
+vim.o.autoread = true
+vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
+	command = "if mode() != 'c' | checktime | endif",
+	pattern = { "*" },
 })
