3d63b1c1115176d93ddbbcafa3283fc9aef4bd41
Mauro Longano <mauro.longano@marconirovereto.it>
2024-04-25 10:16:03 +0200

    Fix and update rest plugin

--- lua/plugins/rest.lua
+++ lua/plugins/rest.lua
@@ -1,56 +1,67 @@
 -- plugins/rest.lua
 return {
-	"rest-nvim/rest.nvim",
-	dependencies = { { "nvim-lua/plenary.nvim" } },
-	config = function()
-		require("rest-nvim").setup({
-			-- Open request results in a horizontal split
-			result_split_horizontal = false,
-			-- Keep the http file buffer above|left when split horizontal|vertical
-			result_split_in_place = false,
-			-- stay in current windows (.http file) or change to results window (default)
-			stay_in_current_window_after_split = true,
-			-- Skip SSL verification, useful for unknown certificates
-			skip_ssl_verification = true,
-			-- Encode URL before making request
-			encode_url = true,
-			-- Highlight request on run
-			highlight = {
-				enabled = true,
-				timeout = 250,
-			},
-			result = {
-				-- toggle showing URL, HTTP info, headers at top the of result window
-				show_url = true,
-				-- show the generated curl command in case you want to launch
-				-- the same request via the terminal (can be verbose)
-				show_curl_command = true,
-				show_http_info = true,
-				show_headers = true,
-				-- table of curl `--write-out` variables or false if disabled
-				-- for more granular control see Statistics Spec
-				show_statistics = false,
-				html = function(body)
-					return vim.fn.system({ "tidy", "-i", "-q", "-" }, body)
-				end,
-			},
-			-- Jump to request line on run
-			-- jump_to_request = false,
-			env_file = ".env.json",
-			-- custom_dynamic_variables = {},
-			-- yank_dry_run = true,
-			-- search_back = true,
-		})
-		-- <Plug>RestNvim, run the request under the cursor
-		-- <Plug>RestNvimPreview, preview the request cURL command
-		-- <Plug>RestNvimLast, re-run the last request
-		vim.keymap.set("n", "<leader>rr", "<Plug>RestNvim", { desc = "RestNvim run the request under the cursor" })
-		vim.keymap.set(
-			"n",
-			"<leader>rp",
-			"<Plug>RestNvimPreview",
-			{ desc = "RestNvim preview the request cURL command" }
-		)
-		vim.keymap.set("n", "<leader>rl", "<Plug>RestNvimLast", { desc = "RestNvim re-run the last request" })
-	end,
+	{
+		"vhyrro/luarocks.nvim",
+		priority = 1000,
+		config = true,
+		opts = {
+			rocks = { "lua-curl", "nvim-nio", "mimetypes", "xml2lua" },
+		},
+	},
+	{
+		"rest-nvim/rest.nvim",
+		ft = "http",
+		dependencies = { "luarocks.nvim" },
+		config = function()
+			require("rest-nvim").setup({
+				-- Open request results in a horizontal split
+				result_split_horizontal = false,
+				-- Keep the http file buffer above|left when split horizontal|vertical
+				result_split_in_place = false,
+				-- stay in current windows (.http file) or change to results window (default)
+				stay_in_current_window_after_split = true,
+				-- Skip SSL verification, useful for unknown certificates
+				skip_ssl_verification = true,
+				-- Encode URL before making request
+				encode_url = true,
+				-- Highlight request on run
+				highlight = {
+					enabled = true,
+					timeout = 250,
+				},
+				result = {
+					-- toggle showing URL, HTTP info, headers at top the of result window
+					show_url = true,
+					-- show the generated curl command in case you want to launch
+					-- the same request via the terminal (can be verbose)
+					show_curl_command = true,
+					show_http_info = true,
+					show_headers = true,
+					-- table of curl `--write-out` variables or false if disabled
+					-- for more granular control see Statistics Spec
+					show_statistics = false,
+					html = function(body)
+						return vim.fn.system({ "tidy", "-i", "-q", "-" }, body)
+					end,
+				},
+				-- Jump to request line on run
+				-- jump_to_request = false,
+				env_file = ".env.json",
+				-- custom_dynamic_variables = {},
+				-- yank_dry_run = true,
+				-- search_back = true,
+			})
+			-- <Plug>RestNvim, run the request under the cursor
+			-- <Plug>RestNvimPreview, preview the request cURL command
+			-- <Plug>RestNvimLast, re-run the last request
+			vim.keymap.set("n", "<leader>rr", "<Plug>RestNvim", { desc = "RestNvim run the request under the cursor" })
+			vim.keymap.set(
+				"n",
+				"<leader>rp",
+				"<Plug>RestNvimPreview",
+				{ desc = "RestNvim preview the request cURL command" }
+			)
+			vim.keymap.set("n", "<leader>rl", "<Plug>RestNvimLast", { desc = "RestNvim re-run the last request" })
+		end,
+	},
 }
