4994f85a97504c41e3db94b925bc64aa6ce1d153
Mauro Longano <mauro@oruam.org>
2024-10-12 21:17:59 +0200

    Enable hererocks in lazy

--- lua/lazy-init.lua
+++ lua/lazy-init.lua
@@ -1,14 +1,23 @@
 -- lazy install
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
-require("lazy").setup("plugins")
+require("lazy").setup("plugins", {
+	rocks = {
+		enable = true, -- Enable LuaRocks support
+		install = true, -- Automatically install missing rocks
+		clean = true, -- Automatically clean rocks
+		timeout = 300, -- Timeout for LuaRocks operations (in seconds)
+		disable = false, -- Do not disable LuaRocks
+		hererocks = true, -- Enable Hererocks to manage Lua dependencies
+	},
+})
