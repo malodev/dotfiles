c4b9e8c924335c231afdeece6b3ac98e492dce45
Mauro Longano <mauro@oruam.org>
2024-11-25 00:30:42 +0100

    fix: fix lsp complition

--- lua/plugins_notvscode/lsp.lua
+++ lua/plugins_notvscode/lsp.lua
@@ -1,5 +1,5 @@
 return {
-	-- completion part
+	-- Completion part
 	{
 		"L3MON4D3/LuaSnip",
 		lazy = false,
@@ -8,6 +8,23 @@ return {
 			"saadparwaiz1/cmp_luasnip",
 		},
 		config = function()
+			local ls = require("luasnip")
+
+			vim.keymap.set({ "i" }, "<C-K>", function()
+				ls.expand()
+			end, { silent = true })
+			vim.keymap.set({ "i", "s" }, "<C-L>", function()
+				ls.jump(1)
+			end, { silent = true })
+			vim.keymap.set({ "i", "s" }, "<C-J>", function()
+				ls.jump(-1)
+			end, { silent = true })
+
+			vim.keymap.set({ "i", "s" }, "<C-E>", function()
+				if ls.choice_active() then
+					ls.change_choice(1)
+				end
+			end, { silent = true })
 			require("luasnip.loaders.from_vscode").lazy_load()
 		end,
 	},
@@ -26,6 +43,12 @@ return {
 	},
 	{
 		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-cmdline",
+			"hrsh7th/cmp-nvim-lua",
+		},
 		lazy = false,
 		config = function()
 			local cmp = require("cmp")
