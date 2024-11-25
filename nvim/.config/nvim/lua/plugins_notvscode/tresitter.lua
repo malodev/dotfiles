c67609b9aa60fec28b3d9ccb5319993522c70e96
Mauro Longano <mauro.longano@marconirovereto.it>
2024-11-26 00:40:30 +0100

    feat(nushell): add nushell to dotfiles

--- nvim/.config/nvim/lua/plugins_notvscode/tresitter.lua
+++ nvim/.config/nvim/lua/plugins_notvscode/tresitter.lua
@@ -1,32 +1,38 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		local configs = require("nvim-treesitter.configs")
-		configs.setup({
-			ensure_installed = {
-				"c",
-				"lua",
-				"vim",
-				"javascript",
-				"typescript",
-				"tsx",
-				"css",
-				"html",
-				"markdown",
-				"markdown_inline",
-				"bash",
-				"regex",
-				"http",
-				"xml",
-				"graphql",
-				"json",
-				"dockerfile",
-				"astro",
-			},
-			sync_install = false,
-			highlight = { enable = true },
-			indent = { enable = true },
-		})
-	end,
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  dependencies = {
+    -- Additional Nushell parser
+    { "nushell/tree-sitter-nu", build = ":TSUpdate nu" },
+  },
+
+  config = function()
+    local configs = require("nvim-treesitter.configs")
+    configs.setup({
+      ensure_installed = {
+        "c",
+        "lua",
+        "vim",
+        "javascript",
+        "typescript",
+        "tsx",
+        "css",
+        "html",
+        "markdown",
+        "markdown_inline",
+        "bash",
+        "regex",
+        "http",
+        "xml",
+        "graphql",
+        "json",
+        "dockerfile",
+        "astro",
+        "nu"
+      },
+      sync_install = false,
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end,
 }
