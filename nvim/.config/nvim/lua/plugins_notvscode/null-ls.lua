0caaddd7e23c3afc34b7d9768d2bb339db7f5bba
Mauro Longano <mauro@oruam.org>
2024-11-30 18:35:20 +0100

    fix(nvim): temporarily fix null-ls rpc.flush()

--- nvim/.config/nvim/lua/plugins_notvscode/null-ls.lua
+++ nvim/.config/nvim/lua/plugins_notvscode/null-ls.lua
@@ -4,11 +4,16 @@ return {
 	config = function()
 		local null_ls = require("null-ls")
 
+		-- Override the problematic flush function
+		local rpc = require("null-ls.rpc")
+		rpc.flush = function() end -- Replace with empty function
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.prettierd,
 			},
+			update_in_insert = false,
+			debug = false,
 		})
 		vim.keymap.set("n", "<leader>gf", "<cmd>lua vim.lsp.buf.format()<CR>", { noremap = true })
 	end,
