3ffe5670ef2b8a1f7c5e3e46a067ebe7b69ef5a1
Mauro Longano <mauro@oruam.org>
2024-11-30 20:08:36 +0100

    feat(stow): add script to install apps and stow packages based on OS

---
+++ Brewfile
@@ -0,0 +1,24 @@
+brew "neovim"
+
+# command line tools
+brew "fzf"
+brew "ripgrep"
+brew "lsd"
+brew "exa"
+brew "bat"
+brew "fd"
+brew "procs"
+brew "zoxide"
+
+# Shells and terminal
+brew "tmux"
+brew "starship"
+brew "zsh"
+brew "nushell"
+
+# Mac specific apps 
+# tap "FelixKratz/formulae"
+# brew "sketchybar"
+brew "FelixKratz/formulae/sketchybar" if OS.mac?
+cask "nikitabobko/tap/aerospace" if OS.mac?
+
---
+++ install.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# check for homebrew and install if we don't have it
+if test ! $(which brew); then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# update homebrew recipes
+brew update
+
+# check for any missing brews
+brew upgrade
+
+# install all our dependencies with bundle
+brew bundle
+brew bundle --file=~/dotfiles/Brewfile
+
+# Check for Kitty.app and install if we don't have it
+if test ! $(which kitty); then
+  curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+fi
+
+stow nvim
+stow kitty
+stow tmux
+stow starship
+stow nushell
+stow zsh
+
+# get os (linux or macos)
+os=$(uname -s)
+
+# if os is macos do specific actions for macos
+if [ $os = "Darwin" ]; then
+  stow sketchybar-omerxx
+  stow aerospace
+fi
+
+if [ $os = "Linux" ]; then
+  stow i3
+fi
+
+
+
+
