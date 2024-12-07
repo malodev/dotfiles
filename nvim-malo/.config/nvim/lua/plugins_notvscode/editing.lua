a2cf3c074c596a2d29817ff67d5cc7350c1bfa12
Mauro Longano <mauro.longano@marconirovereto.it>
2024-12-07 21:05:04 +0100

    chore(nvim): rename stuff for consistency

--- nvim-malo/.config/nvim/lua/plugins_notvscode/editing.lua
+++ nvim-malo/.config/nvim/lua/plugins_notvscode/editing.lua
@@ -1,29 +1,13 @@
 return {
-	{
-		"windwp/nvim-autopairs", -- plugin 1
-		event = { "InsertEnter" },
-		config = function()
-			local npairs_status_ok, npairs = pcall(require, "nvim-autopairs")
-			if not npairs_status_ok then
-				return
-			end
-
-			npairs.setup({})
-		end,
-	},
+	-- {
+	-- 	"windwp/nvim-autopairs", -- plugin 1
+	-- 	event = { "InsertEnter" },
+	-- 	opts = {},
+	-- },
 	{
 		"windwp/nvim-ts-autotag", -- plugin 2
-		event = { "InsertEnter" },
-
-		config = function()
-			local autotag_status_ok, autotag = pcall(require, "nvim-ts-autotag")
-
-			if not autotag_status_ok then
-				return
-			end
-
-			autotag.setup({})
-		end,
+		event = "InsertEnter",
+		opts = {},
 	},
 	{
 		"numToStr/Comment.nvim",
