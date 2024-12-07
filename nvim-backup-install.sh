53b1d015d6b041f64189fe1245af2be1a1f46525
Mauro Longano <mauro.longano@marconirovereto.it>
2024-12-07 20:59:48 +0100

    feat(dotfiles): improve nvim local install backup in order to switch configs

--- nvim-backup-install.sh
+++ nvim-backup-install.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
-mv ~/.local/share/nvim ~/.local/share/nvim.bak
-mv ~/.local/state/nvim ~/.local/state/nvim.bak
-mv ~/.cache/nvim ~/.cache/nvim.bak
+# Check the arguments
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 <backup-neovim-distribution-name>"
+  # list the available backup names
+  ls -d ~/dotfiles/nvim-* | sed 's/.*\///' | sed 's/nvim-//' |uniq | sort
+  exit 1
+fi
+
+# Backup nvim
+# keep only two backups
+rm -rf ~/.local/share/nvim-$1.bak-1
+rm -rf ~/.local/state/nvim-$1.bak-1
+if [ -d ~/.local/share/nvim-$1.bak ]; then
+  mv ~/.local/share/nvim-$1.bak ~/.local/share/nvim-$1.bak-1
+fi
+if [ -d ~/.local/state/nvim-$1.bak ]; then
+  mv ~/.local/state/nvim-$1.bak ~/.local/state/nvim-$1.bak-1
+fi
+
+mv ~/.local/share/nvim ~/.local/share/nvim-$1.bak
+mv ~/.local/state/nvim ~/.local/state/nvim-$1.bak
 
--- nvim-restore-install.sh
+++ nvim-restore-install.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
-mv ~/.local/share/nvim ~/.local/share/nvim.bak-1
-mv ~/.local/state/nvim ~/.local/state/nvim.bak-1
 
-mv ~/.local/share/nvim.bak ~/.local/share/nvim
-mv ~/.local/state/nvim.bak ~/.local/state/nvim
+# Check the arguments
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 <backup-neovim-distribution-name>"
+  # list the available backup names
+  ls -d ~/.local/share/nvim-*.bak | sed 's/.*\///' | sed 's/\.bak//'| sed 's/nvim-//' |uniq | sort
+  exit 1
+fi
+
+# Restore nvim
+# keep only two backups
+
+if [ -d ~/.local/share/nvim ]; then
+  rm -rf ~/.local/share/nvim
+fi
+
+if [ -d ~/.local/state/nvim ]; then
+  rm -rf ~/.local/state/nvim
+fi
+
+mv ~/.local/share/nvim-$1.bak ~/.local/share/nvim
+mv ~/.local/state/nvim-$1.bak ~/.local/state/nvim
 # mv ~/.cache/nvim ~/.cache/nvim.bak
 
