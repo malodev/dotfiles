b73d6af604c8c9b39b811faad80e4f46894bf823
Mauro Longano <mauro.longano@marconirovereto.it>
2024-12-08 16:53:09 +0100

    feat(nvim-test): add treesitter
    tried commands Inspect and InspectTree
    test the following commands:
    
    vim.cmd [[hi @function.buildin.lua guifg=yellow ]]
    vim.cmd [[hi @comment guifg=yellow ]]

---
+++ nvim-test/.config/nvim-test/lua/config/plugins/treesitter.lua
@@ -0,0 +1,41 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    dependencies = {
+        -- Additional Nushell parser
+        { "nushell/tree-sitter-nu", build = ":TSUpdate nu" },
+    },
+
+    config = function()
+        local configs = require("nvim-treesitter.configs")
+        configs.setup({
+            modules = {},
+            auto_install = true,
+            ignore_install = {},
+            ensure_installed = {
+                "c",
+                "lua",
+                "vim",
+                "javascript",
+                "typescript",
+                "tsx",
+                "css",
+                "html",
+                "markdown",
+                "markdown_inline",
+                "bash",
+                "regex",
+                "http",
+                "xml",
+                "graphql",
+                "json",
+                "dockerfile",
+                "astro",
+                "nu"
+            },
+            sync_install = false,
+            highlight = { enable = true },
+            indent = { enable = true },
+        })
+    end
+}
