9d52612eaa19232d348a397db076d767c1695d0d
Mauro Longano <mauro.longano@marconirovereto.it>
2024-12-15 23:14:50 +0100

    feat(nvim-lazy): add flags to enable disable plugins

---
+++ nvim-lazy/.config/nvim-lazyvim/lua/config/init.lua
@@ -0,0 +1,11 @@
+return {
+	is_enabled = {
+		chatgpt = false,
+		codeium = true,
+		nvchad_colorizer = true,
+		norcalli_colorizer = false,
+		neogit = true,
+		telescope_tabs = true,
+		bufferline = true,
+	},
+}
