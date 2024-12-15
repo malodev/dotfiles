3b9c867c04ba5aaa46fcc1627659266b87a76d7e
Mauro Longano <mauro.longano@marconirovereto.it>
2024-12-15 16:06:26 +0100

    feat(nvim-lazyvim): add LazyVim to neovim configurations

---
+++ nvim-lazy/.config/nvim-lazyvim/lua/plugins/oil.lua
@@ -0,0 +1,30 @@
+return {
+  "stevearc/oil.nvim",
+  -- dependencies = { "echasnovski/mini.icons" },
+  dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+  config = function()
+    require("oil").setup({
+      columns = { "icon" },
+      keymaps = {
+        insert = {
+          ["<C-l>"] = "<cmd>lua require('oil').expand()<CR>",
+          ["<C-h>"] = false,
+          ["<M-h>"] = "actions.select_split",
+          ["<C-.>"] = "actions.toogle_hidden",
+        },
+        view_options = {
+          show_hidden = true,
+        },
+      },
+    })
+    -- Open parent directory in current window
+    vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory in current window" })
+    -- Open parent directory in floating window
+    vim.keymap.set(
+      "n",
+      "<leader>fl",
+      require("oil").toggle_float,
+      { desc = "Oil: Open parent directory in floating window" }
+    )
+  end,
+}
