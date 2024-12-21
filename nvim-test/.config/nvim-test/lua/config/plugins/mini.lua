4f813bd4d37be422cd0cfa122bf7b82db4324cd4
Mauro Longano <mauro.longano@marconirovereto.it>
2024-12-21 10:54:18 +0100

    feat(nvim-test): add lualine and disable mini.statusline

--- nvim-test/.config/nvim-test/lua/config/plugins/mini.lua
+++ nvim-test/.config/nvim-test/lua/config/plugins/mini.lua
@@ -2,8 +2,7 @@ return {
   'echasnovski/mini.nvim',
   version = false,
   config = function()
-    local statusline = require 'mini.statusline'
-    statusline.setup { use_icons = true }
+    -- require('mini.statusline').setup( { use_icons = true } )
     require('mini.icons').setup({})
   end
 }
