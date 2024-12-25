fc877bb60fb5dfa9e331eaef62a6efa421eda315
Mauro Longano <mauro@oruam.org>
2024-12-26 00:45:18 +0100

    feat(nvim-malo): add php to treesitter

--- nvim-malo/.config/nvim-malo/lua/plugins_notvscode/tresitter.lua
+++ nvim-malo/.config/nvim-malo/lua/plugins_notvscode/tresitter.lua
@@ -1,38 +1,46 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  dependencies = {
-    -- Additional Nushell parser
-    { "nushell/tree-sitter-nu", build = ":TSUpdate nu" },
-  },
-
-  config = function()
-    local configs = require("nvim-treesitter.configs")
-    configs.setup({
-      ensure_installed = {
-        "c",
-        "lua",
-        "vim",
-        "javascript",
-        "typescript",
-        "tsx",
-        "css",
-        "html",
-        "markdown",
-        "markdown_inline",
-        "bash",
-        "regex",
-        "http",
-        "xml",
-        "graphql",
-        "json",
-        "dockerfile",
-        "astro",
-        "nu"
-      },
-      sync_install = false,
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
-  end,
+	{
+		"nvim-treesitter/nvim-treesitter",
+		lazy = true,
+		build = ":TSUpdate",
+		config = function()
+			local configs = require("nvim-treesitter.configs")
+			---@diagnostic disable-next-line: missing-fields
+			configs.setup({
+				ensure_installed = {
+					"c",
+					"lua",
+					"vim",
+					"javascript",
+					"typescript",
+					"tsx",
+					"css",
+					"html",
+					"markdown",
+					"markdown_inline",
+					"bash",
+					"regex",
+					"http",
+					"xml",
+					"graphql",
+					"json",
+					"dockerfile",
+					"astro",
+					"php",
+					"nu",
+				},
+				sync_install = false,
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	},
+	{
+		lazy = true,
+		"nushell/tree-sitter-nu",
+		build = ":TSUpdate nu",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+		},
+	},
 }
