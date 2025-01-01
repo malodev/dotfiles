55ac83bd757358b396da008390ee974ac3f2847f
Mauro Longano <mauro.longano@marconirovereto.it>
2025-01-02 00:29:40 +0100

    feat(nvim-malo): add rose-pine colorschemes

--- nvim-malo/.config/nvim-malo/lua/plugins_notvscode/catppuccin.lua
+++ nvim-malo/.config/nvim-malo/lua/plugins_notvscode/catppuccin.lua
@@ -1,48 +1,135 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
-	priority = 1000,
-	-- options for catppuccin. This will automatically call `require("catppuccin").setup(opts)`
-	--   opts = {
-	opts = {
-		flavour = "mocha", -- latte, frappe, macchiato, mocha
-		background = { -- :h background
-			light = "latte",
-			dark = "mocha",
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		-- options for catppuccin. This will automatically call `require("catppuccin").setup(opts)`
+		--   opts = {
+		opts = {
+			flavour = "mocha", -- latte, frappe, macchiato, mocha
+			background = { -- :h background
+				light = "latte",
+				dark = "mocha",
+			},
+			transparent_background = true, -- disables setting the background color.
+			styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+				comments = { "italic" }, -- Change the style of comments
+				conditionals = { "italic" },
+				loops = {},
+				functions = {},
+				keywords = { "italic", "bold" },
+				strings = { "italic" },
+				variables = {},
+				numbers = {},
+				booleans = {},
+				properties = {},
+				types = {},
+				operators = {},
+			},
+			custom_highlights = function(colors)
+				return {
+					LineNrAbove = { fg = colors.overlay0 },
+					LineNr = { fg = colors.peach, bold = true },
+					LineNrBelow = { fg = colors.overlay0 },
+					SignColumn = { bg = colors.none },
+				}
+			end,
+			integrations = {
+				cmp = true,
+				treesitter = true,
+				telescope = { enabled = true },
+				harpoon = true,
+			},
 		},
-		transparent_background = true, -- disables setting the background color.
-		styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-			comments = { "italic" }, -- Change the style of comments
-			conditionals = { "italic" },
-			loops = {},
-			functions = {},
-			keywords = { "italic", "bold" },
-			strings = { "italic" },
-			variables = {},
-			numbers = {},
-			booleans = {},
-			properties = {},
-			types = {},
-			operators = {},
-		},
-		custom_highlights = function(colors)
-			return {
-				LineNrAbove = { fg = colors.overlay0 },
-				LineNr = { fg = colors.peach, bold = true },
-				LineNrBelow = { fg = colors.overlay0 },
-				SignColumn = { bg = colors.none },
-			}
-		end,
-		integrations = {
-			cmp = true,
-			treesitter = true,
-			telescope = { enabled = true },
-			harpoon = true,
-		},
-	},
 
-	config = function(_, opts)
-		require("catppuccin").setup(opts)
-		vim.cmd.colorscheme("catppuccin")
-	end,
+		config = function(_, opts)
+			require("catppuccin").setup(opts)
+			vim.cmd.colorscheme("catppuccin")
+		end,
+	},
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		config = function()
+			require("rose-pine").setup({
+				variant = "auto", -- auto, main, moon, or dawn
+				dark_variant = "main", -- main, moon, or dawn
+				dim_inactive_windows = false,
+				extend_background_behind_borders = true,
+
+				enable = {
+					terminal = true,
+					legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
+					migrations = true, -- Handle deprecated options automatically
+				},
+
+				styles = {
+					bold = true,
+					italic = true,
+					transparency = true,
+				},
+
+				groups = {
+					border = "muted",
+					link = "iris",
+					panel = "surface",
+
+					error = "love",
+					hint = "iris",
+					info = "foam",
+					note = "pine",
+					todo = "rose",
+					warn = "gold",
+
+					git_add = "foam",
+					git_change = "rose",
+					git_delete = "love",
+					git_dirty = "rose",
+					git_ignore = "muted",
+					git_merge = "iris",
+					git_rename = "pine",
+					git_stage = "iris",
+					git_text = "rose",
+					git_untracked = "subtle",
+
+					h1 = "iris",
+					h2 = "foam",
+					h3 = "rose",
+					h4 = "gold",
+					h5 = "pine",
+					h6 = "foam",
+				},
+
+				palette = {
+					-- Override the builtin palette per variant
+					-- moon = {
+					--     base = '#18191a',
+					--     overlay = '#363738',
+					-- },
+				},
+
+				highlight_groups = {
+					-- Comment = { fg = "foam" },
+					-- VertSplit = { fg = "muted", bg = "muted" },
+				},
+
+				before_highlight = function(group, highlight, palette)
+					-- Disable all undercurls
+					-- if highlight.undercurl then
+					--     highlight.undercurl = false
+					-- end
+					--
+					-- Change palette colour
+					-- if highlight.fg == palette.pine then
+					--     highlight.fg = palette.foam
+					-- end
+				end,
+			})
+
+			-- vim.cmd("colorscheme rose-pine")
+			vim.cmd("colorscheme rose-pine-main")
+			-- vim.cmd("colorscheme rose-pine-moon")
+			-- vim.cmd("colorscheme rose-pine-dawn")
+		end,
+	},
 }
