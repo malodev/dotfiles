721ba373a8907edc670ae4a90266d426d97c4cc3
Mauro Longano <mauro.longano@marconirovereto.it>
2025-01-02 00:05:22 +0100

    fix(nvim-malo): fix telescope

--- nvim-malo/.config/nvim-malo/lua/plugins_notvscode/telescope.lua
+++ nvim-malo/.config/nvim-malo/lua/plugins_notvscode/telescope.lua
@@ -1,138 +1,84 @@
 return {
 	{
 		"nvim-telescope/telescope.nvim",
-		-- tag = "0.1.5",
+		tag = "0.1.8",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 			"nvim-lua/popup.nvim",
 			"folke/trouble.nvim",
 			"nvim-telescope/telescope-media-files.nvim",
-			"nvim-telescope/telescope-smart-history.nvim",
+			"nvim-tree/nvim-web-devicons",
+			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+			"andrew-george/telescope-themes",
 		},
 		config = function()
-			local open_with_trouble = require("trouble.sources.telescope").open
-
-			-- Use this to add more results without clearing the trouble list
-			-- local add_to_trouble = require("trouble.sources.telescope").add
-
 			require("telescope").setup({
-				vimgrep_arguments = {
-					"rg",
-					"--color=never",
-					"--no-heading",
-					"--with-filename",
-					"--line-number",
-					"--column",
-					"--smart-case",
-					"--hidden",
-				},
-				pickers = {
-					find_files = {
-						file_ignore_patterns = {
-							"node_modules",
-							".git",
-							".DS_Store",
-							".venv",
-							".pytest_cache",
-							".mypy_cache",
-							".idea",
-							".vscode",
-						},
-						hidden = true,
-					},
-					live_grep = {
-						file_ignore_patterns = {
-							"node_modules",
-							".git",
-							".DS_Store",
-							".venv",
-							".pytest_cache",
-							".mypy_cache",
-							".idea",
-							".vscode",
-						},
-						additional_args = function(_)
-							return { "--hidden" }
-						end,
-						-- hidden = true,
-					},
-				},
-				extensions = {
-					"fzf",
-					-- prosession = {
-					-- 	path = vim.fn.stdpath("data") .. "/prosession",
-					-- 	ignore = {
-					-- 		"node_modules",
-					-- 		".git",
-					-- 	},
-					-- },
-					smart_history = {
-						path = vim.fn.stdpath("data") .. "/telescope_smart_history.sqlite3",
-					},
-					"rest",
-					noice = {
-						layout_config = {
-							preview_width = 0.65,
-						},
-					},
-					media_files = {
-						-- filetypes whitelist
-						-- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
-						-- filetypes = { "png", "webp", "jpg", "jpeg" },
-						-- find command (defaults to `fd`)
-						find_cmd = "rg",
-					},
-				},
 				defaults = {
-					history = {
-						path = vim.fn.stdpath("data") .. "/telescope_history.sqlite3",
-						limit = 100,
-					},
 					mappings = {
 						i = {
-							["<c-t>"] = open_with_trouble,
 							["<c-j>"] = require("telescope.actions").cycle_history_next,
 							["<c-k>"] = require("telescope.actions").cycle_history_prev,
 						},
-						n = { ["<c-t>"] = open_with_trouble },
 					},
-					layout_config = {
-						horizontal = {
-							preview_cutoff = 0,
-						},
+				},
+				pickers = {
+					find_files = {
+						hidden = true,
+						theme = "dropdown",
+					},
+					grep_string = {
+						additional_args = { "--hidden" },
+					},
+					live_grep = {
+						additional_args = { "--hidden" },
+					},
+				},
+				extensions = {
+					fzf = {
+						fuzzy = true, -- false will only do exact matching
+						override_generic_sorter = true, -- override the generic sorter
+						override_file_sorter = true, -- override the file sorter
+						case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+						hidden = true,
 					},
 				},
 			})
-			require("telescope").load_extension("smart_history")
-			require("telescope").load_extension("fzf")
-			-- require("telescope").load_extension("prosession")
-			require("telescope").load_extension("media_files")
-			-- require("telescope").load_extension("noice")
-			require("telescope").load_extension("rest")
+			local telescope = require("telescope")
+			telescope.load_extension("fzf")
+			-- telescope.load_extension("prosession")
+			telescope.load_extension("media_files")
+			telescope.load_extension("rest")
+			telescope.load_extension("themes")
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "buffers" })
+			vim.keymap.set("n", "<leader>fd", builtin.lsp_document_symbols, { desc = "lsp document symbols" })
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "find files" })
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "live grep" })
+			vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "help tags" })
+			vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "Find keymaps" })
+			vim.keymap.set("n", "<leader>fo", builtin.oldfiles, { desc = "oldfiles" })
+			vim.keymap.set("n", "<leader>fr", builtin.lsp_references, { desc = "lsp references" })
+			vim.keymap.set(
+				"n",
+				"<leader>tp",
+				":Telescope themes<CR>",
+				{ noremap = true, silent = true, desc = "Theme Switcher" }
+			)
+			vim.keymap.set("n", "<leader>fw", builtin.lsp_workspace_symbols, { desc = "lsp workspace symbols" })
+			-- vim.keymap.set("n", "<leader>fc", builtin.lsp_code_actions, { desc = "lsp code actions" })
 		end,
 		keys = {
-			{ "<leader>ff", "<cmd>FzfLua files<CR>", desc = "telescope: find_files" },
-			{ "<leader>fg", "<cmd>FzfLua live_grep<CR>", desc = "telescope: live_grep" },
-			{ "<leader>fb", "<cmd>Telescope buffers<CR>", desc = "telescope: buffers" },
-			{ "<leader>fo", "<cmd>Telescope oldfiles<CR>", desc = "telescope: oldfiles" },
-			{ "<leader>fc", "<cmd>Telescope command_history<CR>", desc = "telescope: command_history" },
-			{ "<leader>pf", "<cmd>Telescope prosession<CR>", desc = "telescope: sessions" },
-			{ "<leader>fp", "<cmd>Telescope prosession<CR>", desc = "telescope: sessions" },
-			{ "<leader>fi", "<cmd>Telescope media_files<CR>", desc = "telescope: media_files" },
-			{ "<leader>fn", "<cmd>Telescope noice<CR>", desc = "telescope: noice" },
-			{ "<leader>fr", "<cmd>Telescope rest select_env<CR>", desc = "telescope: select rest env" },
-			{ "<leader>/", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Buffer search" },
-			-- { "<leader>fc", "<cmd>Telescope git_commits<cr>", desc = "Commits" },
 			{ "<C-p>", "<cmd>Telescope git_files<cr>", desc = "Git files" },
-			-- { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help" },
-			{ "<leader>fj", "<cmd>Telescope command_history<cr>", desc = "History" },
-			{ "<leader>fk", "<cmd>Telescope keymaps<cr>", desc = "Keymaps" },
+			{ "<leader>/", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Buffer search" },
+			{ "<leader>fc", "<cmd>Telescope command_history<CR>", desc = "telescope: command_history" },
+			{ "<leader>fe", "<cmd>Telescope rest select_env<CR>", desc = "Select rest env" },
+			{ "<leader>fi", "<cmd>Telescope media_files<CR>", desc = "telescope: media_files" },
 			{ "<leader>fl", "<cmd>Telescope lsp_references<cr>", desc = "Lsp References" },
+			-- { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help" },
 			{ "<leader>fs", "<cmd>Telescope grep_string<cr>", desc = "Grep String" },
 			{ "<leader>ft", "<cmd>Telescope treesitter<cr>", desc = "Treesitter" },
 		},
 	},
-	{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 	{
 		"nvim-telescope/telescope-ui-select.nvim",
 		config = function()
