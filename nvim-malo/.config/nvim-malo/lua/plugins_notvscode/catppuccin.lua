d83c83d63b8e0f8fcfbfe6961e2d5081d61bc23e
Mauro Longano <mauro.longano@marconirovereto.it>
2025-01-12 00:38:21 +0100

    feat(nvim-malo): add kanagawa theme and key maps to set themes

--- nvim-malo/.config/nvim-malo/lua/plugins_notvscode/catppuccin.lua
+++ nvim-malo/.config/nvim-malo/lua/plugins_notvscode/catppuccin.lua
@@ -1,3 +1,6 @@
+vim.keymap.set("n", "<leader>tk", "<cmd>colorscheme kanagawa-dragon<cr>")
+vim.keymap.set("n", "<leader>tc", "<cmd>colorscheme catppuccin<cr>")
+vim.keymap.set("n", "<leader>tr", "<cmd>colorscheme rose-pine-moon<cr>")
 return {
 	{
 		"catppuccin/nvim",
@@ -127,9 +130,39 @@ return {
 			})
 
 			-- vim.cmd("colorscheme rose-pine")
-			vim.cmd("colorscheme rose-pine-main")
+			-- vim.cmd("colorscheme rose-pine-main")
 			-- vim.cmd("colorscheme rose-pine-moon")
 			-- vim.cmd("colorscheme rose-pine-dawn")
+		end,
+	},
+	{
+		"rebelot/kanagawa.nvim",
+		config = function()
+			require("kanagawa").setup({
+				compile = false, -- enable compiling the colorscheme
+				undercurl = true, -- enable undercurls
+				commentStyle = { italic = true },
+				functionStyle = {},
+				keywordStyle = { italic = true },
+				statementStyle = { bold = true },
+				typeStyle = {},
+				transparent = true, -- do not set background color
+				dimInactive = true, -- dim inactive window `:h hl-NormalNC`
+				terminalColors = true, -- define vim.g.terminal_color_{0,17}
+				colors = { -- add/modify theme and palette colors
+					palette = {},
+					theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+				},
+				overrides = function(colors) -- add/modify highlights
+					return {}
+				end,
+				theme = "dragon", -- Load "wave" theme when 'background' option is not set
+				background = { -- map the value of 'background' option to a theme
+					dark = "dragon", -- try "dragon" !
+					light = "lotus",
+				},
+			})
+			vim.cmd("colorscheme kanagawa-dragon")
 		end,
 	},
 }
