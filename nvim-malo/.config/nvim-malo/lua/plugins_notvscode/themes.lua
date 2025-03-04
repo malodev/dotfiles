0cb37f04320111dd926a7ae759f70bd781e8de24
Mauro Longano <mauro.longano@marconirovereto.it>
2025-03-04 17:06:54 +0100

    feat(nvim-malo): update rosé pine color for current line number
    already done for catppuccin in thos commit:
    
    https://github.com/malodev/dotfiles/commit/d0bac1c4c7928250e31267bb3fb955d3aedc1f78#diff-046f1dc6a3075f0cc30f32164b0f37e20deba2f82ae645cee310858aa439d448

--- nvim-malo/.config/nvim-malo/lua/plugins_notvscode/themes.lua
+++ nvim-malo/.config/nvim-malo/lua/plugins_notvscode/themes.lua
@@ -133,6 +133,9 @@ return {
 			-- vim.cmd("colorscheme rose-pine-main")
 			vim.cmd("colorscheme rose-pine-moon")
 			-- vim.cmd("colorscheme rose-pine-dawn")
+			vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "grey" })
+			vim.api.nvim_set_hl(0, "LineNr", { fg = "orange" })
+			vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "grey" })
 		end,
 	},
 	{
