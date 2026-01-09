ed8811291944ebd28ffa73dee8c73b5b52b04ccc
Mauro Longano <mauro.longano@marconirovereto.it>
2026-01-09 17:31:32 +0100

    feat(lsp): suppress deprecation warnings for client.supports_method
    This commit modifies the behavior of the LSP clients to suppress
    deprecation warnings related to the method "client.supports_method".
    It overrides the default behavior of `vim.deprecate` and `vim.notify`
    to prevent specific warnings from being displayed.
    
    This change aims to improve user experience by reducing noise from
    unnecessary warnings in the LSP integration.

--- nvim-malo/.config/nvim-malo/init.lua
+++ nvim-malo/.config/nvim-malo/init.lua
@@ -1,3 +1,26 @@
+-- Suppress LSP deprecation warnings from upstream plugins (must be first!)
+do
+	-- Override vim.deprecate
+	local original_deprecate = vim.deprecate
+	vim.deprecate = function(name, alternative, version, plugin, message)
+		if name == "client.supports_method" then
+			return
+		end
+		if original_deprecate then
+			original_deprecate(name, alternative, version, plugin, message)
+		end
+	end
+
+	-- Also override vim.notify as backup
+	local original_notify = vim.notify
+	vim.notify = function(msg, level, opts)
+		if type(msg) == "string" and msg:find("client.supports_method is deprecated") then
+			return
+		end
+		original_notify(msg, level, opts)
+	end
+end
+
 require("options")
 require("lazy-init")
 require("keymaps")
